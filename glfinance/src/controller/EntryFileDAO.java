4b99b5a8723b287befce1cd5ee146c9d99a67d5b
Greg Lynskey <gclynskey@outlook.com>
2022-12-19 14:19:26 -0500

    Added EntryFileDAO.java stubbed out necessary methods

---
+++ glfinance/src/controller/EntryFileDAO.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+
+import model.Entry;
+import model.category;
+import model.subcategory;
+
+public class EntryFileDAO implements EntryDAO{
+
+    @Override
+    public Entry[] get_all_entries() throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry get_entry(int id) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry[] find_all_by_category(category c) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry[] find_all_by_subcategory(subcategory s) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry create_entry(Entry entry) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry update_entry(int id, Entry entry) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean delete_entry(int id) throws IOException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    
+}
